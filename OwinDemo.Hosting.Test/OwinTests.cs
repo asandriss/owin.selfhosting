f669547bee037acb20f908104a94c3e3f3a96602
asandriss <asandriss@outlook.com>
2016-08-06 23:27:56 +0200

    In memory hosting for unit tests

---
+++ OwinDemo.Hosting.Test/OwinTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using Microsoft.Owin.Testing;
+using System.Net;
+
+namespace OwinDemo.Hosting.Test
+{
+    [TestClass]
+    public class OwinTests
+    {
+        [TestMethod]
+        public async Task Owin_returns_200_on_request_to_root()
+        {
+            using (var server = TestServer.Create<Startup>())
+            {
+                var response = await server.HttpClient.GetAsync("/");
+                // TODO: Validate response
+
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        public async Task Owin_returns_hello_world_on_request_to_root()
+        {
+            using (var server = TestServer.Create<Startup>())
+            {
+                var response = await server.HttpClient.GetAsync("/");
+                // TODO: Validate response
+
+                var body = await response.Content.ReadAsStringAsync();
+                Assert.AreEqual("Hello World", body);
+            }
+        }
+    }
+}
